aca79c6801d3e4970c8fa235d2891fea4b606b4f
Fatima <fatimavaliyeva0704@gmail.com>
2024-07-17 17:32:47 +0400

    Add project files.

---
+++ Custom List hw 17-07/CustomList.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Threading.Channels;
+
+namespace Custom_List_hw_17_07
+{
+    public class CustomList<T> : IEnumerable<T>
+    {
+
+        private T[] lists;
+        public CustomList()
+        {
+            lists = new T[0];
+        }
+
+        public T this[int index]
+        {
+            get
+              => lists[lists.Length - 1 - index];
+            set
+              => lists[lists.Length - 1 - index] = value;
+        }
+
+        public void Add(T item)
+        {
+            Array.Resize(ref lists, lists.Length + 1);
+            lists[^1] = item;
+        }
+
+        public T Find(T item)
+        {
+            foreach (var t in lists)
+            {
+                if (t.Equals(item))
+                {
+                    return t;
+                }
+            }
+            return default(T); 
+        }
+
+        public CustomList<T> FindAll(Predicate<T> find)
+        {
+            CustomList<T> list = new CustomList<T>();
+            foreach (var t in lists)
+            {
+                if (t.Equals(find))
+                {
+                    list.Add(t);
+                }
+            }
+            return list;
+        }
+
+        public void Remove(T item)
+        {
+            for (int i = 0; i < lists.Length; i++)
+            {
+                if (item.Equals(lists[i]))
+                {
+                    for (int j = i; j < lists.Length - 1; j++)
+                    {
+                        lists[j] = lists[j + 1];
+                    }
+                }
+            }
+            Array.Resize(ref lists, lists.Length - 1);
+        }
+
+        public void RemoveAll(Predicate<T> remove)
+        {
+            Array.Resize(ref lists, 0);
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = lists.Length - 1; i >= 0; i--)
+            {
+                yield return lists[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = lists.Length - 1; i >= 0; i--)
+            {
+                yield return lists[i];
+            }
+        }
+    }
+}
---
+++ Custom List hw 17-07/Program.cs
@@ -0,0 +1,43 @@
+﻿using Custom_List_hw_17_07;
+
+CustomList<int> list = new CustomList<int>();
+
+list.Add(1);
+list.Add(2);
+list.Add(3);
+list.Add(4);
+list.Add(5);
+list.Add(6);
+list.Add(7);
+
+Console.WriteLine("Here is the list: ");
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("\nTask: finding element 3");
+Console.WriteLine(list.Find(3));
+
+Console.WriteLine("\nTask: Finding all elements");
+CustomList<int> foundElements = list.FindAll(x => true);
+foreach(var element in foundElements)
+{
+    Console.WriteLine(element);
+}
+
+Console.WriteLine("\nTask: Removing element 5");
+list.Remove(5);
+Console.WriteLine("result:");
+foreach (var element in list)
+{
+    Console.WriteLine(element);
+}
+
+Console.WriteLine("\nTask: Removing all elements");
+list.RemoveAll(x => true);
+Console.WriteLine("result: ");
+foreach (var element in list)
+{
+    Console.WriteLine(element);
+}
